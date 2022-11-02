70b5f94f5fddc332cbe40439cc1c2bd8248b9189
[Akash Shukla] <[akashshukla7458@gmail.com]>
2022-11-02 22:22:41 +0530

    final commit

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const express= require('express')
-const authorize = require('./authorize')
 const app= express()
-const logger= require('./logger')
+let {people}= require('./data')
+//static assests
+app.use(express.static('./methods-public'))
+//parse form data
+app.use(express.urlencoded({extended:false}))
+//parse json
+app.use(express.json())
 
-app.use([logger,authorize])
-app.get('/',(req,res)=>{
-    res.send('Home')
-})
-app.get('/about',(req,res)=>{
- res.send('About')
-})
-app.get('/api/products',(req,res)=>{
-    res.send('Products')
-})
-app.get('/api/items',(req,res)=>{
-    console.log(req.user)
-    res.send('Items')
-})
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people })
+  })
 
-
-app.listen(5000, ()=>{
-    console.log('server is listening on port 5000')
+app.post('/api/people',(req,res)=>{
+    const{name}=req.body
+if(!name){
+    return res.status(400).json({success: false, msg:'please provide name value'})
+}
+res.status(201).json({success: true, person:name})
 })
